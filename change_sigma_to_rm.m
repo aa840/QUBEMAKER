2b882eec17272c7e9c3f3c55b17404a8e52b5f8f
aa840 <aa840@cam.ac.uk>
2018-08-25 19:01:35 +0100

    Delete change_sigma_to_rm.m

--- change_sigma_to_rm.m
+++
@@ -1,8 +0,0 @@
-function [ rm ] = change_sigma_to_rm( sigma )
-%The charm file format has a different set of parameters to the OPLS format
-%this function changes the sigma from OPLS to rm charm format 
- 
-rm = (2 *  ( sigma^(6) ))^(1/6) /2; %division by 2 as rmin/2 in charm file 
-
-end
-
